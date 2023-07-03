e91d78a1e8e94f13cd3a400ca395c182504c52c7
Nahu <nahu20dev@gmail.com>
2023-07-03 19:15:45 -0300

    configure color picker functions

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 
 let grid = document.querySelector(".grid");
-let length = 50;
-let margin = 10;
-let numbers = 5;
+let length = 30;
+let margin = 1;
+let numbers = 20;
+let sliderLength = 350;
 let percentageArr = Array(numbers).fill().map(x => Array(numbers).fill(0));
-let color1 = "white";
+let color1 = "red";
 let color2 = "blue";
 let slider1 = document.getElementById("color1-slider");
 let gradient1 = document.getElementById("color1-gradient"); 
 let gradient2 = document.getElementById("color2-gradient"); 
-let picker1 = document.getElementById("picker1");
-let picker2 = document.getElementById("picker2");
-let animating = false;
-let requestId = 0;
+let sPicker1 = document.getElementById("spicker1");
+let sPicker2 = document.getElementById("spicker2");
+let gPicker1 = document.getElementById("gpicker1");
+let gPicker2 = document.getElementById("gpicker2");
 
 for(let i = 0 ; i < numbers*numbers ; i++){
     let square= grid.appendChild( document.createElement("div"));
@@ -20,11 +21,12 @@ for(let i = 0 ; i < numbers*numbers ; i++){
     square.style.width = length.toString() + "px";
     square.style.height = length.toString() + "px";
     square.style.backgroundColor = color1;
-    square.style.margin = "10px";
+    square.style.margin = margin.toString() + "px";
     square.addEventListener("mouseover", changeSquareColor);
 }
 
 grid.style.width = (numbers*length + 2*numbers*margin).toString() + "px";
+grid.style.height = (numbers*length + 2*numbers*margin).toString() + "px";
 
 function changeSquareColor(event){
     let x,y;
@@ -34,35 +36,37 @@ function changeSquareColor(event){
 }
 
 function slider(n,event){
-    event.stopPropagation();
-    requestId = requestAnimationFrame( _ => sliderAnimation(n,event.offsetX,event.buttons));
-}
-
-function sliderAnimation(n ,x,buttons){
-    if(buttons % 2 === 0 || x <= 2){
+    if(event.buttons % 2 === 0 || event.target.className == "sliderpicker" ){
         return;
     }
-    let deg = Math.floor(x*360/400);
+    event.stopPropagation();
+    let deg = Math.floor(event.offsetX*360/sliderLength);
     if(n === 1){
         gradient1.style.backgroundColor = `hsl( ${deg}deg , 100% , 50%)`;
-        picker1.style.marginLeft = `${x}px`;
+        sPicker1.style.marginLeft = `${event.offsetX}px`;
     }else{
         gradient2.style.backgroundColor = `hsl( ${deg}deg , 100% , 50%)`;
-        picker2.style.marginLeft = `${x}px`;
+        sPicker2.style.marginLeft = `${event.offsetX}px`;
     }
 }
 
 function gradient(n,event){
-    if(event.buttons % 2 === 0){
+    if(event.buttons % 2 === 0 || event.target.className == "gradientpicker"){
         return;
     }
+    event.stopPropagation();
     if(n === 1){
-        console.log(event.offsetX);
-        let deg = Math.floor(Number(picker1.style.marginLeft.match(/\d+/g))*360/400);
-        color1 = `hsl( ${deg}deg , ${Math.floor(event.offsetX/4)}% , ${100-Math.floor(event.offsetY/4)}% )`;
+        let deg = Math.floor(Number(sPicker1.style.marginLeft.match(/\d+/g))*360/sliderLength);
+        color1 = `hsl( ${deg}deg , ${Math.floor(event.offsetX*100/sliderLength)}% , ${100-Math.floor(event.offsetY*100/sliderLength)}% )`;
+        gPicker1.style.marginLeft = `${event.offsetX - 5}px`;
+        gPicker1.style.marginTop = `${event.offsetY - 5}px`;
+        gPicker1.style.backgroundColor = color1;
     }else{
-        let deg = Math.floor(Number(picker2.style.marginLeft.match(/\d+/g))*360/400);
-        color2 = `hsl( ${deg}deg , ${Math.floor(event.offsetX/4)}% , ${100-Math.floor(event.offsetY/4)}% )`;
+        let deg = Math.floor(Number(sPicker2.style.marginLeft.match(/\d+/g))*360/sliderLength);
+        color2 = `hsl( ${deg}deg , ${Math.floor(event.offsetX*100/sliderLength)}% , ${100-Math.floor(event.offsetY*100/sliderLength)}% )`;
+        gPicker2.style.marginLeft = `${event.offsetX - 5}px`;
+        gPicker2.style.marginTop = `${event.offsetY - 5}px`;
+        gPicker2.style.backgroundColor = color2;
     }
     requestAnimationFrame(updateGrid);
 }
